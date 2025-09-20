119e6fc3278dc98ffefff7884380115ed36ceccf
Sanjana C <sanjanac@Chbis-MacBook-Pro.local>
2025-09-21 00:20:25 +0530

    added navbar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,44 @@ import "./App.css";
 import Threads from "./components/Threads";
 import ProjectSlider from "./components/ProjectSlider";
 import SkillsSection from "./components/SkillSection";
-import ExperienceTimeline from "./components/ExperienceTimeline";
 
 export default function App() {
   return (
     <div className="relative min-h-screen bg-[#0d1117] text-white overflow-x-hidden">
-      <div className="relative z-10 flex flex-col">
-        <Navbar />
-        <HeroSection />
-        <div
-          style={{
-            width: "100%",
-            height: "900px",
-            position: "fixed",
-            pointerEvents: "none",
-          }}
-        >
-          <Threads
-            amplitude={1.5}
-            distance={0}
-            enableMouseInteraction={true}
-            color={[0.5, 0, 1]} // 🔴 Red threads
-          />
-        </div>
+      
+      {/* Background Threads */}
+      <div className="fixed inset-0 pointer-events-none z-0">
+        <Threads
+          amplitude={1.5}
+          distance={0}
+          enableMouseInteraction={true}
+          color={[0.5, 0, 1]}
+        />
+      </div>
 
-        <AboutSection />
-        <ProjectSlider />
-        <SkillsSection />
-        <ExperienceTimeline />
+      {/* Foreground content */}
+      <div className="relative z-10 flex flex-col">
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Sections */}
+        <section id="home">
+          <HeroSection />
+        </section>
+
+        <section id="about">
+          <AboutSection />
+        </section>
+
+        <section id="projects">
+          <ProjectSlider />
+        </section>
+
+        <section id="skills">
+          <SkillsSection />
+        </section>
+
       </div>
     </div>
   );
